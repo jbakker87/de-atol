881403c553346aa5dd29d1834c9a67d3a9a34591
Jeroen <s1114449@student.windesheim.nl>
2020-08-17 01:41:38 +0200

    veerdienst basic page set up

--- src/app/diensten/veerdienst/veerdienst.component.ts
+++ src/app/diensten/veerdienst/veerdienst.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeerdienstComponent implements OnInit {
 
+  adultPrice = 7;
+  childPrice = 4;
+  bikePrice = 1;
+
   constructor() { }
 
   ngOnInit(): void {
