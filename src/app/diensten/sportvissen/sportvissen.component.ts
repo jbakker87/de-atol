77c0b481a6da08c10a031cf444b717d460ee0f01
Jeroen <s1114449@student.windesheim.nl>
2020-09-07 13:54:20 +0200

    sportvissen updates

--- src/app/diensten/sportvissen/sportvissen.component.ts
+++ src/app/diensten/sportvissen/sportvissen.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SportvissenComponent implements OnInit {
 
+
+  dayPrice = 540;
+  hengelPrice = 10;
+  baitPrice = 10;
+
   constructor() { }
 
   ngOnInit(): void {
