332cffc3b830cd985f05cd5b1b0cc5a6ffec21ce
BakkerJeroen <jbakker01@outlook.com>
2021-04-09 01:02:22 +0200

    new 2021 veerdienst schedule made with mat-paginator

--- src/app/vaardagen/veerdienst-hansweert/veerdienst-hansweert.component.ts
+++ src/app/vaardagen/veerdienst-hansweert/veerdienst-hansweert.component.ts
@@ -1,35 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-veerdienst-hansweert',
   templateUrl: './veerdienst-hansweert.component.html',
   styleUrls: ['./veerdienst-hansweert.component.css']
 })
-export class VeerdienstHansweertComponent implements OnInit {
-  columnsToDisplay: string[] = ['text', 'time1', 'time2', 'time3'];
-  scheduleSource = new MatTableDataSource<HansweertData>(scheduleHansweertData);
-  dataSource: MatTableDataSource<HansweertData>;
+export class VeerdienstHansweertComponent implements AfterViewInit {
+  columnsToDisplay: string[] = ['date', 'time1', 'time2', 'time3'];
+  scheduleHansweertSource = new MatTableDataSource<HansweertData>(scheduleHansweertData);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   constructor() {
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(scheduleHansweertData);
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.scheduleHansweertSource.paginator = this.paginator;
   }
 }
 
 export interface HansweertData {
-  text: string;
+  date: Date;
   time1: string;
   time2: string;
   time3: string;
 }
 
 const scheduleHansweertData: HansweertData[] = [
-  { text: 'Zondag', time1: '11:00', time2: '12:00', time3: '17:00' },
-  { text: 'Maandag', time1: '11:00', time2: '11:00', time3: '17:00' },
-  { text: 'Woensdag', time1: '11:00', time2: '12:00', time3: '17:00' },
-  { text: 'Vrijdag', time1: '11:00', time2: '12:00', time3: '17:00' }
+  { date: new Date(2021, 6, 4),  time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 6, 5),  time1: '10:00', time2: '12:00', time3: '17:00' }, // maandag
+  { date: new Date(2021, 6, 7),  time1: '10:00', time2: '12:00', time3: '17:00' }, // woensdag
+  { date: new Date(2021, 6, 9),  time1: '10:00', time2: '12:00', time3: '17:00' }, // vrijdag
+  { date: new Date(2021, 6, 11), time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 6, 12), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 14), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 16), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 18), time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 6, 19), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 21), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 4),  time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 25), time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 6, 26), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 28), time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 6, 4),  time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 7, 1),  time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 7, 2),  time1: '10:00', time2: '12:00', time3: '17:00' },
+  { date: new Date(2021, 7, 4),  time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 6),  time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 8),  time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag 
+  { date: new Date(2021, 7, 9),  time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 11), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 13), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 15), time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 7, 16), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 18), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 20), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 22), time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 7, 23), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 25), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 27), time1: '10:00', time2: '12:00', time3: '17:00' }, 
+  { date: new Date(2021, 7, 29), time1: '10:00', time2: '12:00', time3: '17:00' }, // zondag
+  { date: new Date(2021, 7, 30), time1: '10:00', time2: '12:00', time3: '17:00' },
+  // { text: 'Zondag', time1: '11:00', time2: '12:00', time3: '17:00' },
+  // { text: 'Maandag', time1: '11:00', time2: '11:00', time3: '17:00' },17:00
+  // { text: 'Woensdag', time1: '11:00', time2: '12:00', time3: '17:00' }17:00
+  // { text: 'Vrijdag', time1: '11:00', time2: '12:00', time3: '17:00' }
 ]
--- src/app/vaardagen/veerdienst-perkpolder/veerdienst-perkpolder.component.ts
+++ src/app/vaardagen/veerdienst-perkpolder/veerdienst-perkpolder.component.ts
@@ -1,35 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-veerdienst-perkpolder',
   templateUrl: './veerdienst-perkpolder.component.html',
   styleUrls: ['./veerdienst-perkpolder.component.css']
 })
-export class VeerdienstPerkpolderComponent implements OnInit {
-  columnsToDisplay: string[] = ['text', 'time1', 'time2', 'time3'];
+export class VeerdienstPerkpolderComponent implements AfterViewInit {
+  columnsToDisplay: string[] = ['date', 'time1', 'time2', 'time3'];
   scheduleSource = new MatTableDataSource<PerkpolderData>(schedulePerkpolderData);
-  dataSource: MatTableDataSource<PerkpolderData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   constructor() {
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(schedulePerkpolderData);
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.scheduleSource.paginator = this.paginator;
+
   }
 }
 
 export interface PerkpolderData {
-  text: string;
+  date: Date;
   time1: string;
   time2: string;
   time3: string;
 }
 
 const schedulePerkpolderData: PerkpolderData[] = [
-  { text: 'Zondag', time1: '09:00', time2: '11:00', time3: '16:15' },
-  { text: 'Maandag', time1: '09:00', time2: '11:00', time3: '16:15' },
-  { text: 'Woensdag', time1: '09:00', time2: '11:00', time3: '16:15' },
-  { text: 'Vrijdag', time1: '09:00', time2: '11:00', time3: '16:15' }
+  { date: new Date(2021, 6, 4), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 6, 5), time1: '09:00', time2: '11:00', time3: '16:15' }, // maandag
+  { date: new Date(2021, 6, 7), time1: '09:00', time2: '11:00', time3: '16:15' }, // woensdag
+  { date: new Date(2021, 6, 9), time1: '09:00', time2: '11:00', time3: '16:15' }, // vrijdag
+  { date: new Date(2021, 6, 11), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 6, 12), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 14), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 16), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 18), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 6, 19), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 21), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 4), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 25), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 6, 26), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 28), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 6, 4), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 7, 1), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 7, 2), time1: '09:00', time2: '11:00', time3: '16:15' },
+  { date: new Date(2021, 7, 4), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 6), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 8), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag 
+  { date: new Date(2021, 7, 9), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 11), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 13), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 15), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 7, 16), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 18), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 20), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 22), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 7, 23), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 25), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 27), time1: '09:00', time2: '11:00', time3: '16:15' }, 
+  { date: new Date(2021, 7, 29), time1: '09:00', time2: '11:00', time3: '16:15' }, // zondag
+  { date: new Date(2021, 7, 30), time1: '09:00', time2: '11:00', time3: '16:15' },
 ];
